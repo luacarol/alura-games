da2919af09e7068468e7af07c7f4e4a7455181df
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-14 15:38:14 -0300

    info api shark

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -2,8 +2,8 @@ package org.example
 
 import com.google.gson.annotations.SerializedName
 
-class Jogo(@SerializedName("title") val titulo:String,
-           @SerializedName("thumb") val capa:String) {
+class Jogo(val titulo:String,
+           val capa:String) {
     val descricao = "Jogo ..."
 
     override fun toString(): String {
